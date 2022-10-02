3f44fd74dbf189e101342ed35a3b8ca1e76e4aae
Lionel COTE <grcote7@gmail.com>
2022-10-02 18:19:47 +0200

    Feat/Gulp + Browser Reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-const { watch, src, dest, series } = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const { watch, src, dest, series } = require('gulp');
 
 function scss() {
   return src('assets/css/style.scss')
@@ -7,15 +10,30 @@ function scss() {
     .pipe(dest('assets/css/'))
 }
 
-function build(cb) {
-  cb();
-}
-
 function helloTask() {
   console.log('hello');
 }
 
+// Watch scss AND html files, doing different things with each.
+function serve() {
+
+  // Serve files from the root of this project
+  browserSync.init({
+    
+      proxy: "defi_online"
+    
+      // server: {
+      //     baseDir: "./"
+      // }
+  })
+
+  gulp.watch(["*/**/*.html", "*/**/*.css", "*/**/*.twig"]).on("change", reload);
+}
+
 exports.build = series;
+
 exports.default = function () {
   watch('assets/css/style.scss', scss);
+  helloTask();
+  serve();
 }
