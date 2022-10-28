8a755f705b8108eda170c1dcbf920b563b5cb922
Lionel COTE <grcote7@gmail.com>
2022-10-28 13:49:10 +0200

    Fixg: Vite PHP

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+// vite.config.js
+export default  defineConfig({
+  build: {
+    // generate manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: './main.js'
+    }
+  }
+})
